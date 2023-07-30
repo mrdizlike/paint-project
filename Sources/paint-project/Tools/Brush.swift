b6fbe250083884d4948644c29220f500488075f8
mrdizlike <billchiper12@gmail.com>
2023-07-30 17:21:00 +0300

    Dynamic line FIX
    -Fixed dynamic line creation
    -Fixed dynamic smooth (again)

--- Sources/paint-project/Tools/Brush.swift
+++ Sources/paint-project/Tools/Brush.swift
@@ -24,6 +24,7 @@ class Brush: DrawProtocol {
     var isFirst = true
     
     //Переменные для физики кисти
+    let lerpFactor: CGFloat = 0.98
     var dynamicBrushSize: CGFloat = 0
     var previousTimestamp: TimeInterval?
     var previousSize: CGFloat = 0
@@ -43,6 +44,9 @@ class Brush: DrawProtocol {
     
     func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?, _ view: DrawingView) {
         let newPoint = touches.first!.location(in: view)
+        maxSize = size
+        minSize = maxSize / 2
+        
         initBezierPath()
         bezierPath.path.move(to: newPoint)
         
@@ -66,9 +70,8 @@ class Brush: DrawProtocol {
         let distance = sqrt(pow(newPoint.x - (lastPoint!.x), 2) + pow(newPoint.y - (lastPoint!.y), 2))
         let speed = distance / CGFloat(deltaTime)
         
-        let minSpeed: CGFloat = 100
+        let minSpeed: CGFloat = 10
         let maxSpeed: CGFloat = 1000.0
-        maxSize = size
         
         let normalizedSpeed = max(min(speed, maxSpeed), minSpeed) / maxSpeed
         let brushSizeRange = maxSize - minSize
@@ -77,34 +80,34 @@ class Brush: DrawProtocol {
             let pressure = (touch?.force)!
             dynamicBrushSize = CGFloat(pressure)
         } else {
-            previousSize = maxSize - (brushSizeRange * normalizedSpeed)
+            dynamicBrushSize = maxSize - (brushSizeRange * normalizedSpeed)
         }
         
         // Степень сглаживания между линиями
-        let lerpFactor: CGFloat = 0.9
         dynamicBrushSize = lerp(start: previousSize, end: dynamicBrushSize, t: lerpFactor)
         
         if let prevPoint = lastPoint {
-            // Кубическая линия безье
-            let midPoint1 = CGPoint(x: (prevPoint.x + newPoint.x) / 2, y: (prevPoint.y + newPoint.y) / 2)
-            let midPoint2 = CGPoint(x: (midPoint1.x + newPoint.x) / 2, y: (midPoint1.y + newPoint.y) / 2)
+            // Квадратичная линия безье
+            let midPoint = CGPoint(x: (newPoint.x + prevPoint.x) / 2, y: (newPoint.y + prevPoint.y) / 2)
             
-            initBezierPath()
+            if Int(previousSize) != Int(dynamicBrushSize) {
+                bezierPath.path.addQuadCurve(to: midPoint, controlPoint: prevPoint)
+                initBezierPath()
+                bezierPath.path.move(to: prevPoint)
+                bezierPath.path.lineWidth = dynamicBrushSize
+                previousSize = dynamicBrushSize
+                
+                let lastIndex = view.linePaths.indices.last!
+                view.linePaths[lastIndex].lines.append(bezierPath)
+            }
             
-            bezierPath.path.move(to: prevPoint)
             bezierPath.path.lineWidth = dynamicBrushSize
-            bezierPath.path.addCurve(to: midPoint2, controlPoint1: midPoint1, controlPoint2: midPoint1)
-            bezierPath.path.addCurve(to: newPoint, controlPoint1: midPoint2, controlPoint2: midPoint2)
+            bezierPath.path.addQuadCurve(to: midPoint, controlPoint: prevPoint)
             
-            previousSize = dynamicBrushSize
         }
-        
         lastPoint = newPoint
         previousTimestamp = currentTimestamp
-        
-        let lastIndex = view.linePaths.indices.last!
-        
-        view.linePaths[lastIndex].lines.append(bezierPath)
+
         view.setNeedsDisplay()
     }
     
